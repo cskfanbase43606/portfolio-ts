487dc2423f6d269760e649cefb6bd86912804c6a
CSKFanbase07 <102754810+cskfanbase43606@users.noreply.github.com>
2025-05-18 19:03:21 +0530

    Update index.tsx

--- src/common/components/sections/about/index.tsx
+++ src/common/components/sections/about/index.tsx
@@ -46,7 +46,7 @@ export default function About() {
             <div className="text-md relative z-40 flex flex-col gap-3 font-semibold tracking-wide text-primary lg:absolute lg:right-0 lg:top-[27%] lg:block lg:max-w-[580px] lg:text-start lg:text-lg xl:top-1/3 xl:h-[442px] xl:max-w-[650px]">
               <div className="flex h-full flex-col justify-center gap-6">
                 <span>
-                  I&apos;m Front-end developer with expertise in TypeScript,
+                  I&apos;m a Front-end developer with expertise in TypeScript,
                   React & Next.js. Proven ability to deliver user-centered web
                   applications with a focus on intuitive experiences and user
                   interaction.
