1e3f4b3a395472ee7d973988452ad5ebd5d48db6
CSKFanbase07 <102754810+cskfanbase43606@users.noreply.github.com>
2025-06-06 21:48:28 +0530

    Update index.tsx

--- src/common/components/sections/hero/index.tsx
+++ src/common/components/sections/hero/index.tsx
@@ -72,7 +72,7 @@ export default function Hero() {
 
               <a
                 className="borderBlack group flex w-64 cursor-pointer items-center justify-center gap-2 rounded-full bg-white px-7 py-3 text-black outline-none transition hover:bg-gray-100 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:w-auto"
-                href="https://drive.google.com/file/d/1DQru21O_5REy4eIe2gQ90tH_rzwTxg23/view?usp=sharing"
+                href="https://drive.google.com/drive/my-drive?q=after:2025-06-06%20parent:0AP5o1fQmxRsKUk9PVA"
                 download
               >
                 <span>Download CV</span>
